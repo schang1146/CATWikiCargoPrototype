eb158c59891b2dcb7dbdc4dcf29893035dc13089
Wayne Keenan <wayne.keenan@gmail.com>
2025-05-23 19:35:13 +0100

    Add default data and supporting scripts

---
+++ scripts/export_db.sh
@@ -0,0 +1,1 @@
+docker-compose exec mariadb sh -c 'exec mysqldump --single-transaction --routines --triggers -u"wikiuser" -p"secret1976" "mediawiki"' | gzip > data/mediawiki_export.sql.gz
--- scripts/import_default_pages.sh
+++ scripts/import_default_pages.sh
@@ -1,6 +1,4 @@
 
-source .env
-gunzip -c docker.db_image/mediawiki-initial.sql.gz | docker-compose exec -T mariadb mysql -u"wikiuser" -p"secret1976" "mediawiki"
-cat docker/wiki_image/default_pages.xml | docker-compose exec -T mediawiki php maintenance/run.php importDump.php --
-docker-compose exec -T mediawiki php maintenance/run.php rebuildrecentchanges.php --quiet
-docker-compose exec -T mediawiki php maintenance/run.php initSiteStats.php --quiet
+cat data/default_pages.xml | docker-compose exec -T mediawiki php maintenance/run.php importDump.php --
+docker-compose exec -T mediawiki php maintenance/run.php rebuildrecentchanges.php
+docker-compose exec -T mediawiki php maintenance/run.php initSiteStats.php
